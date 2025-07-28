78c226611d76092e03f32b23156f916954abd862
SuperPsycho4347 <superpsycho4347@gmail.com>
2025-07-29 03:05:27 +0500

    initial commit

---
+++ script.js
@@ -0,0 +1,23 @@
+const url = "https://api.breakingbadquotes.xyz/v1/quotes"
+const authorDiv = document.querySelector('.author')
+const quoteDiv = document.querySelector('.quote')
+
+
+fetchQuote(url)
+
+
+async function fetchQuote(url) {
+    const response = await fetch(url);
+    const responseJSON = await response.json();
+    const quote = (responseJSON[0].quote)
+    const author = (responseJSON[0].author)
+
+    displayQuote(author, quote)
+}
+
+function displayQuote(author, quote) {
+    const authorText = document.createElement('h1');
+    // console.log(author)
+    authorText.textContent = author;
+    authorDiv.appendChild(authorText)
+}
